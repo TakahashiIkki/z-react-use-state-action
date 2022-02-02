20df92bcb0ded8bf9b99eb3151d40d51a8da71bf
Takashi Ikki <new_ittyandesu@outlook.jp>
2022-02-03 00:11:53 +0900

    add group state

--- react-hooks-in-action/src/components/Bookables/BookablesList.js
+++ react-hooks-in-action/src/components/Bookables/BookablesList.js
@@ -1,21 +1,49 @@
 import { useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
 import * as staticJson from "../../static.json";
 const { bookables } = staticJson;
 
-export function BookablesList() {
-  const group = "Rooms";
-  const bookablesInGroup = bookables.filter((b) => b.group === group);
-  const [bookableIndex, setBookableIndex] = useState(1);
+function getUniqueValues(array, property) {
+  const propValues = array.map((element) => element[property]);
+  const uniqueValues = new Set(propValues);
+  const uniqueValuesArray = [...uniqueValues];
 
+  return uniqueValuesArray;
+}
+
+export function BookablesList() {
+  const [group, setGroup] = useState("Kit");
+  const bookablesInGroup = bookables.filter((b) => b.group === group);
+  const [bookableIndex, setBookableIndex] = useState(0);
+  const groups = getUniqueValues(bookables, "group");
+
+  function nextBookable() {
+    setBookableIndex((i) => (i + 1) % bookablesInGroup.length);
+  }
   return (
-    <ul className="bookables items-list-nav">
-      {bookablesInGroup.map((b, i) => (
-        <li key={b.id} className={i === bookableIndex ? "selected" : null}>
-          <button className="btn" onClick={() => setBookableIndex(i)}>
-            {b.title}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <select value={group} onChange={(e) => setGroup(e.target.value)}>
+        {groups.map((g) => (
+          <option value={g} key={g}>
+            {g}
+          </option>
+        ))}
+      </select>
+      <ul className="bookables items-list-nav">
+        {bookablesInGroup.map((b, i) => (
+          <li key={b.id} className={i === bookableIndex ? "selected" : null}>
+            <button className="btn" onClick={() => setBookableIndex(i)}>
+              {b.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <p>
+        <button className="btn" onClick={nextBookable} autoFocus>
+          <FaArrowRight />
+          <span>Next</span>
+        </button>
+      </p>
+    </div>
   );
 }
