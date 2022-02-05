926435e5dceebe6eedcc69b8503a79e0542e728b
Takashi Ikki <new_ittyandesu@outlook.jp>
2022-02-05 22:54:43 +0900

    user の情報を表示するように

--- react-hooks-in-action/src/components/Users/UsersList.js
+++ react-hooks-in-action/src/components/Users/UsersList.js
@@ -4,16 +4,31 @@ const { users } = staticJson;
 
 export function UsersList() {
   const [userIndex, setUserIndex] = useState(1);
+  const selectedUser = users[userIndex];
 
   return (
-    <ul className="bookables items-list-nav">
-      {users.map((user, i) => (
-        <li key={user.id} className={i === userIndex ? "selected" : null}>
-          <button className="btn" onClick={() => setUserIndex(i)}>
-            {user.name}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="bookables items-list-nav">
+        {users.map((user, i) => (
+          <li key={user.id} className={i === userIndex ? "selected" : null}>
+            <button className="btn" onClick={() => setUserIndex(i)}>
+              {user.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+      {selectedUser && (
+        <div className="item user">
+          <div className="item-header">
+            <h2>{selectedUser.name}</h2>
+          </div>
+
+          <div className="user-details">
+            <h3>{selectedUser.title}</h3>
+            <p>{selectedUser.notes}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
